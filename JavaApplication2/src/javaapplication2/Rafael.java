904ec43a7dab3ea2674a85385447e82cba7c8d41
rafa22232 <rafael.ferreira347@etec.sp.gov.br>
2025-02-12 22:20:00 -0300

    Add files via upload

---
+++ JavaApplication2/src/javaapplication2/Rafael.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author RAFAEL_4770
+ */
+public class Rafael {
+    public static void main(String[] args){
+        String nome = "Rafael";
+        System.out.println("Seu nome é: "+ nome);
+    }
+    
+}
